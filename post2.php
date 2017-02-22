602747b2ee1ee0b9a51b060053196b2b302fd09b
Charalampos Nikitopoulos <nikitopoul@ceid.upatras.gr>
2017-02-23 00:41:01 +0200

    create

--- post2.php
+++
@@ -1,7 +0,0 @@
-<?php
-header('Content-type: text/plain; charset=utf-8');
-#ΕΜΦΑΝΙΣΗ ΑΠΟΛΥΤΗΣ ΤΙΜΗΣ
-
-		$api_response = file_get_contents("http://localhost/myapi1.php?request1=1&ripos2=".$_POST["ripos2"]."&kod_stathmou=".$_POST["kod_stathmou"]."&date=".$_POST["date"]."&time=".$_POST["time"]."&api=687e161606decab91fac285c54ec8442");
-		print $api_response;	
-	?>
--- post3.php
+++
@@ -1,8 +0,0 @@
-<?php
-header('Content-type: text/plain; charset=utf-8');
-#ΕΜΦΑΝΙΣΗ ΣΤΑΘΜΩΝ
-
-		$api_response = file_get_contents("http://localhost/myapi1.php?request2=1&ripos3=".$_POST["ripos3"]."&stathmos3=".$_POST["stathmos3"]."&imerominia3=".$_POST["imerominia3"]."&api=687e161606decab91fac285c54ec8442");
-		
-		print $api_response;	
-	?>
